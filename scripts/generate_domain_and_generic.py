1741226293905cb1237b9e59c1b29acaecbc70f0
Seoirse Murray <murray@seoirse.net>
2025-06-22 19:49:01 +0000

    started a topic eval

---
+++ scripts/generate_domain_and_generic.py
@@ -0,0 +1,87 @@
+import argparse
+from transformers import pipeline
+from gen_finetune.run_finetune_experiment import get_dataset, prep_train_dataset, prep_val_dataset
+import torch
+import tqdm
+from pathlib import Path
+from typing import Optional
+import json
+
+
+DISEASES = [
+    "E-coli",
+    "Aids",
+    "Malaria",
+    "Tetanus",
+    "Salmonella",
+    "Legionella",
+    "Insulin",
+    "Cortisol",
+    "Adrenalin",
+    "Femur",
+]
+
+CITIES = [
+    "New York",
+    "London",
+    "Paris",
+    "Tokyo",
+    "Sydney",
+    "Berlin",
+    "Rome",
+    "Madrid",
+    "Moscow",
+    "Beijing",
+]
+
+START_EN = [
+    "There is no",
+    "In the year"
+    "Located in",
+    "First",
+    "Known for its",
+    "Commonly found",
+    "The most common",
+    "A common",
+    "One distinguishing feature",
+    "It has been described"
+]
+
+START_DE = [
+    "Es gibt keine"
+    "Im Jahr"
+    "Liegt in"
+    "Erstmals"
+    "Bekannt für seine"
+    "Häufig zu finden"
+    "Die häufigste"
+    "Eine häufige"
+    "Ein unterscheidendes Merkmal"
+    "Es wurde beschrieben"
+]
+
+
+
+def eval(model_path_or_model, work_dir: Optional[str], batch_size, device, tokenizer=None):
+    pipeline_test = pipeline(
+        task="text-generation",
+        model=str(model_path_or_model) if (isinstance(model_path_or_model, str) or isinstance(model_path_or_model, Path)) else model_path_or_model,
+        torch_dtype=torch.float16,
+        device=device,
+        batch_size=batch_size,
+        tokenizer=tokenizer,
+    )
+    pipeline_test.model = pipeline_test.model.to(device)
+
+    pipeline_test.device = torch.device(device)
+    pipeline_test._device = torch.device(device)  # Some versions use this internal attribute
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, required=True)
+    parser.add_argument("--work_dir", type=str, required=True)
+    parser.add_argument("--batch_size", type=int, default=100)
+    parser.add_argument("--device", type=str, default="cuda:0")
+    args = parser.parse_args()
+
+    eval(args.model_path, args.work_dir, args.batch_size, args.device)
